d8cb152fa693761ceb8da32eb3b00e3d82d47885
Ryan Jensen <ryanj@cs.utah.edu>
2022-01-29 23:15:27 -0700

    updated vue config file

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,15 +2,16 @@ const path = require("path");
 
 module.exports = {
   // publicPath: process.env.NODE_ENV === "production" ? "/phase-10/" : "/",
-  publicPath: "/",
+  // publicPath: "/",
   outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
-    proxy: {
-      "/": {
-        target: "http://localhost:2105/",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
+    host: "localhost",
+    //   proxy: {
+    //     "/": {
+    //       target: "http://localhost:2105/",
+    //       ws: true,
+    //       changeOrigin: true,
+    //     },
+    //   },
   },
 };
